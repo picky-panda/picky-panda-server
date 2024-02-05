0d2e1cd4d971fc3d4edd73eb10220a58d7c16599
dev-Crayon <seungheon328@gmail.com>
2024-02-05 22:44:51 +0900

    Project initializing

---
+++ src/main/java/io/picky/panda/PickyPandaApplication.java
@@ -0,0 +1,13 @@
+package io.picky.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickyPandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickyPandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/picky/panda/PickyPandaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.picky.panda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickyPandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
