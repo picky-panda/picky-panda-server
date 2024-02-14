accea622d336ab6498582b852e714583e3e8ea62
dev-Crayon <seungheon328@gmail.com>
2024-02-15 03:03:28 +0900

    H2 제거

--- src/main/java/io/picky/panda/config/SecurityConfig.java
+++ src/main/java/io/picky/panda/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import io.picky.panda.security.filter.ExceptionHandlerFilter;
 import io.picky.panda.security.filter.JwtCustomFilter;
 import io.picky.panda.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -41,7 +40,6 @@ public class SecurityConfig {
                 .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
                                 .requestMatchers(
                                         Stream
                                                 .of(WHITE_LIST)
